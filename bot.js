a88a0bcdad533cbc8ebea325e374df7743d314a1
fahmednoion <fahmednoion@gmail.com>
2025-09-27 23:58:21 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,21 @@ const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix line breaks
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+    type: "service_account",
+    project_id: "prizebondbot",  // Not sensitive
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,  // Sensitive
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Sensitive
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,  // Sensitive
+    client_id: "112933415710381597512",  // Not sensitive
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",  // Not sensitive
+    token_uri: "https://oauth2.googleapis.com/token",  // Not sensitive
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",  // Not sensitive
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40prizebondbot.iam.gserviceaccount.com"  // Not sensitive
   }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  databaseURL: "https://prizebondbot-default-rtdb.asia-southeast1.firebasedatabase.app/"  // Not sensitive
 });
 
 const db = admin.database();
-
 
 // Prize bond draw data (directly included in the code)
 const prizeBondDraws = [
@@ -404,3 +403,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
