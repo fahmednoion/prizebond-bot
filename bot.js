4e5c802c9ae605e309729b8e611fcbe1a68e1a96
fahmednoion <fahmednoion@gmail.com>
2025-09-27 15:00:48 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -335,16 +335,16 @@ bot.onText(/\/help/, (msg) => {
     const chatId = msg.chat.id;
     const helpMessage = `
 📌 **Available Commands:**
-/storeprizebond <numbers> - Store your prize bond numbers (comma-separated, up to 10 digits each).
+/storeprizebond <numbers> - Store your prize bond numbers (comma-separated).
 /myprizebond - View your stored prize bond numbers.
 /delete <numbers> - Delete specific prize bond numbers (comma-separated) or use "all" to delete all.
 /checkmyprizebond - Check your stored prize bond numbers against all draws.
-/check <numbers> - Check specific prize bond numbers (comma-separated, up to 10 digits each) against all draws.
+/check <numbers> - Check specific prize bond numbers (comma-separated) against all draws.
 /draws - List all available prize bond draws.
 /draw <draw_number> - View details of a specific draw (e.g., /draw 111).
 /author - Know about the author.
 /help - Show this help message.
-♻️ If you find the chatbot inactive, please visit 👉 https://prizebond-bot.onrender.com 👈 to activate it. ♻️
+♻️ To inactive chatbot inactive, please visit 👉 https://prizebond-bot.onrender.com 👈 
     `;
     bot.sendMessage(chatId, helpMessage, { parse_mode: 'Markdown' });
 });
@@ -376,4 +376,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
